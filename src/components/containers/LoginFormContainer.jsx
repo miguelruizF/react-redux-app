43648f11b4acb4a2ee515109aefc664961e3d961
Miguel Ruiz <later481@gmail.com>
2024-10-20 18:36:38 -0600

    function mapState and mapDispatch finished

--- src/components/containers/LoginFormContainer.jsx
+++ src/components/containers/LoginFormContainer.jsx
@@ -1,3 +1,5 @@
+import { connect } from "react-redux"
+import { httpRequest } from "../../store/actions/asyncActions"
 
 
 const mapStateToProps = (state) => {
@@ -6,3 +8,19 @@ const mapStateToProps = (state) => {
         fetching: state.userState.fetching
     }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogin: (email, password) => {
+            const data = {
+                email: email,
+                password: password
+            }
+            const url = 'https://reqres.in/api/login'
+            dispatch(httpRequest('POST', url, data));
+        }
+    }
+}
+
+const LoginFormContainer = connect(mapStateToProps, mapDispatchToProps)
+export default LoginFormContainer
